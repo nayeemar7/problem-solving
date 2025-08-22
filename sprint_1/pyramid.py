c0bcbce7b2d538f630a7ad8b615ed3f837778cd6
Nayeem Ahamed <nayeemar.ahamed@gmail.com>
2025-08-22 15:20:06 +0000

    To Print numeric pyramid pattern using nested loops

---
+++ sprint_1/pyramid.py
@@ -0,0 +1,8 @@
+def pyramid(row):
+    for i in range(1,row+1):
+        for j in range(1,i+1):
+            print(j,end= " ")
+        print()
+
+row = int(input("Enter the number of rows: "))
+pyramid(row)
